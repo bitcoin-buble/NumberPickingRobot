2da43efa433647b1c805729168e95e146aabe2e8
Bitcoin Buble <bitcoin.buble@gmail.com>
2018-10-04 14:37:32 +0100

    make P Money's life easier and save the robot's recordings each time

--- robot.sh
+++ robot.sh
@@ -1,16 +1,17 @@
+#!/bin/bash
 
-node robot.js intro | say
+node robot.js intro | tee >(say) | say -o "./output/intro.aiff"
 read -n 1
-node robot.js wew | say
+node robot.js wew | tee >(say) | say -o "./output/wew.aiff"
 read -n 1
-node robot.js intro2 | say
+node robot.js intro2 | tee >(say) | say -o "./output/intro2.aiff"
 read -n 1
-node robot.js verify | say
+node robot.js verify | tee >(say) | say -o "./output/verify.aiff"
 read -n 1
-node robot.js contestants | say
+node robot.js contestants | tee >(say) | say -o "./output/contestants.aiff"
 read -n 1
-node robot.js compIntro | say
+node robot.js compIntro | tee >(say) | say -o "./output/compIntro.aiff"
 read -n 1
-node robot.js winnerIntro | say
+node robot.js winnerIntro | tee >(say) | say -o "./output/winnerIntro.aiff"
 read -n 1
-node robot.js winner | say
+node robot.js winner | tee >(say) | say -o "./output/winner.aiff"
