937ddc00d4fa831cd81162690118612f245a19fa
Bitcoin Buble <bitcoin.buble@gmail.com>
2019-02-27 09:21:15 +0000

    pick some coins to pick from

--- robot.js
+++ robot.js
@@ -1,89 +1,34 @@
-const fs = require("fs");
 const axios = require("axios");
-const moment = require("moment");
-const hash = require("hash.js");
-const { flow, slice, join, reverse, get, times } = require("lodash/fp");
-const contestants = [
-  "CryptoAnubis",
-  "Irishbeastman25",
-  "Jeffrey Crawford",
-  "Kap kap kap Kap zilla",
-  // "Lavisse",
-  "Maarm",
-  "Myphatarz",
-  "Pepper Plant Pants",
-  "Swampy Bits",
-  "William Bitting",
-  "Big Crypto Dave"
-];
+const {
+  sample,
+  flow,
+  orderBy,
+  slice,
+  sampleSize,
+  reject,
+  map,
+  join
+} = require("lodash/fp");
 
-const sponsor = "Beers for Brett";
+const pickCoins = async () => {
+  const { data } = await axios.get("https://api.coinpaprika.com/v1/coins");
 
-const verifyAuthenticityOfDraw = async () => {
-  const { data: info } = await axios.get(
-    "https://api.blockcypher.com/v1/eth/main"
-  );
-
-  const lastBitOfHash = flow(
-    reverse,
-    slice(0, 6),
-    reverse,
+  const coins = flow(
+    reject({ rank: 0 }),
+    orderBy(["rank"], ["asc"]),
+    slice(0, 100),
+    sampleSize(4),
+    map("name"),
     join(", ")
-  )(info.hash);
-
-  const output = `The date and time sponsored by ${sponsor} is ${moment().format(
-    "dddd, MMMM Do YYYY, h:mm:ss"
-  )} (UK time). The current block height on eeth ear ree yum is ${
-    info.height
-  } with a hash ending in ${lastBitOfHash}.`;
-
-  fs.writeFileSync("./hash.txt", info.hash);
-
-  // return output;
-  return "This week's draw is sponsored by our HORSE div ease! The time is the time, the date the date. whatever.";
-};
-
-const pickWinner = () => {
-  const blockHash = fs.readFileSync("./hash.txt", "utf8");
-
-  let winner;
-  let nonce = 0;
-
-  while (!winner) {
-    const strToHash = `${blockHash}-${nonce}`;
-
-    const hashOfHash = hash
-      .sha256()
-      .update(strToHash)
-      .digest("hex");
-
-    const char = slice(0, 1, hashOfHash);
-
-    winner = get(char, contestants);
-    nonce++;
-  }
-
-  return `Ken, do the honours and give ${winner} a t shirt will you.`;
-};
-
-const readContestants = () => {
-  const contestantsStr = join(", ", contestants).replace(/,(?=[^,]*$)/, " and");
-  // return `Our contestants this week are ${contestantsStr}.`;
-  return "Fuck's sake. Our contestants this week are the same as last week. Come on you scum bags. Man's got to get paid";
+  )(data);
+  return coins;
 };
 
 const steps = {
   intro: () =>
-    "It's late. Ken's coin went on too long. The wine's finished. Teather is fucked and. Roo beanie is a nob head so I can't be fucked with all the spiel today lads. Also, Boobs has got to go pack so he can get his surf on tomorrow.",
-  intro2: () => "",
-  intro3: () => "",
-  verify: verifyAuthenticityOfDraw,
-  contestants: readContestants,
-  compIntro: () => "Cun testants, are you ready?",
-  // winnerIntro: () => `${times(() => "dah. dah. dah. dagger,", 3)}. hashimoto.`,
-  winnerIntro: () =>
-    `You know the drill. Dah dah dah. dagger. And all that jazz.`,
-  winner: pickWinner
+    "Well, that was a bit short notice but here's four random coins:",
+
+  pickCoins: pickCoins
 };
 
 const run = async step => {
--- robot.sh
+++ robot.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-node robot.js intro | tee >(say) | say -o "./output/intro.aiff"
-# read -n 1
-# node robot.js intro2 | tee >(say) | say -o "./output/intro2.aiff"
-# read -n 1
-# node robot.js intro3 | tee >(say) | say -o "./output/intro3.aiff"
+node robot.js intro | tee >(say)
 read -n 1
-node robot.js verify | tee >(say) | say -o "./output/verify.aiff"
+node robot.js pickCoins | tee >(say)
 read -n 1
-node robot.js contestants | tee >(say) | say -o "./output/contestants.aiff"
-read -n 1
-node robot.js compIntro | tee >(say) | say -o "./output/compIntro.aiff"
-read -n 1
-node robot.js winnerIntro | tee >(say) | say -o "./output/winnerIntro.aiff"
-read -n 1
-node robot.js winner | tee >(say) | say -o "./output/winner.aiff"
