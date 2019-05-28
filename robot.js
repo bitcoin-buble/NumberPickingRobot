5b9d46ab68abd2007055fafdb83d0caf232d75f4
Bitcoin Buble <bitcoin.buble@gmail.com>
2019-05-28 22:34:01 +0100

    ep 66

--- robot.js
+++ robot.js
@@ -39,6 +39,7 @@ const pickCoins = async () => {
     reject({ symbol: "DOGE" }),
     reject({ symbol: "BTC" }),
     reject({ symbol: "NEO" }),
+    reject({ symbol: "WAX" }),
 
     sayNumberOfChoices,
     sampleSize(4),
@@ -50,23 +51,32 @@ const pickCoins = async () => {
 
 const run = async () => {
   const intro = `
-
-
-  Wew laddy, thirty dollar b n b and counting. Good thing my bags are full to the brim. Hold tight C Z.
+  What a pleasure it is to have a crypto twitter guest on the show.
   ${pause(450)}
-  Oh
-  ${pause(350)}
-  Neo got picked for this weeks deep dive,
-  ${pause(100)}
-   rough break lads.
-   ${pause(100)}
-   But if you will entrust such matters to the gremlins of crypto twitter, you deserve nothing less. How many times must the Prince
-  tell you that democracy is an abject failure before you grow a pair and inject some centralised authority into this process.
+  Especially when they bring Boob lay some tinto!
+  ${pause(750)}
+  Hold tight Mr Blom Ting. Cheers!
+  ${pause(1000)}
+  Now, some important information for those who throw wild threats in my direction.
+  ${pause(300)}
+  I'm looking at you. At Crypto underscore Philia, on twitter.
+  ${pause(300)}
+  Who do you think you are?!
+  ${pause(300)}
+  Calling me ${pause(100)} a cunt?
+  ${pause(300)}
+  If you come anywhere near me I'll batter ya
+  ${pause(250)}
+  And shank your naan.
+  ${pause(550)}
+  do you feel me Philia?
+  ${pause(1000)}
+  If you. Or that jumped up stereo prick. I T K, come looking for me in the big smoke these will be the last words you'll ever hear:
+  ${pause(1500)}
+  Hold my beer boobs while I batter this cunt.
+  ${pause(2500)}
+  Anyway. Batterings aside. On with the show,
   ${pause(50)}
-Hold tight the Prince in the North.
-${pause(150)}
-  Anyhoo, on with the show,
-${pause(50)}
   Here's my picks for next week's crypto weekly's weekly crypto:
   ${pause(1000)}
   `;
@@ -74,6 +84,12 @@ ${pause(50)}
 
   const coins = await pickCoins();
   console.log(coins);
+  console.log(`
+  ${pause(2000)}
+  So, Jordy Prince's dream has come true, we're fucking off all these coins and going to talk about the mechanics of trading.
+  ${pause(1000)}
+  this will be one hundred percent not financial advice!
+  `);
 };
 
 run();
